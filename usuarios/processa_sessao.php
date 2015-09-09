3dcecdc532cc2ee034fd46d3c94ca8cb8ea04fb7
Eduardo Azevedo <eduardoazevedo3@gmail.com>
2015-09-09 20:52:25 -0300

    Estrutura para gravar usuários em arquivo texto

---
+++ usuarios/processa_sessao.php
@@ -0,0 +1,48 @@
+<?php
+
+// Desativar os erros do tipo NOTICE
+error_reporting( E_ALL ^ E_NOTICE );
+
+class UsuarioProcessaSessao {
+    
+    public function __construct() {
+        if ( method_exists( $this, $_GET['acao'] ) )
+            call_user_func ( array($this, $_GET['acao']) );
+        else
+            die( "A ação <b>{$_GET['acao']}</b> não existe!" );
+    }
+    
+    // Salvar usuários
+    public function salvar() {
+        // Armazenar usuario na variável
+        $usuario = $_POST;
+
+        // Validar os dados
+        $cpf = $usuario['cpf'];
+
+        if ( strlen($cpf) != 11 || ! is_numeric($cpf) )
+            die( 'Digite um CPF válido' );
+
+        // Iniciar a sessão
+        session_start();
+        $_SESSION['usuarios'][$cpf] = $usuario;
+        
+        // Redirecionar para a página lista.php
+        header( 'Location: lista.php' );
+    }
+    
+    // Excluir usuários
+    public function excluir() {
+        // Iniciar a sessão
+        session_start();
+        
+        // Remover usuário pelo cpf
+        $cpf = $_GET['cpf'];
+        unset( $_SESSION['usuarios'][$cpf] );
+        
+        // Redirecionar para a página lista.php
+        header( 'Location: lista.php' );
+    }
+    
+}
+new UsuarioProcessa();
